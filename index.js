6725039b34d610176332845a2160f98ee773b293
Rondinelle Walitte <rondinellewalitte@gmail.com>
2022-02-20 12:02:31 -0300

    Subindo servidor

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.post('/', (req, res) => {
     const token = req.headers.authorization;
